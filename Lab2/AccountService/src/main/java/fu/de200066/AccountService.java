7a48cca3bfb833342402e865b0938a07fd03c1d7
Bao Huy <ngo17574@gmail.com>
2026-09-23 14:40:32 +0700

    feat(Step 7):AccountServiceTest

---
+++ Lab2/AccountService/src/main/java/fu/de200066/AccountService.java
@@ -0,0 +1,20 @@
+package fu.de200066;
+import java.util.regex.Pattern;
+public class AccountService {
+    private static final Pattern EMAIL_PATTERN =
+            Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Zaz]{2,}$");
+    public boolean isValidEmail(String email) {
+        if (email == null || email.isBlank()) {
+            return false;
+        }
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
+    public boolean registerAccount(String username, String password, String
+            email) {
+        if (username == null || username.isBlank()) return false;
+        if (password == null || password.length() <= 6) return false;
+        if (!isValidEmail(email)) return false;
+        // (Demo) — bỏ qua phần lưu DB
+        return true;
+    }
+}
